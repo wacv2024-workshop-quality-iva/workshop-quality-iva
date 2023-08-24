dbb81be2a7bab12f0bec5dacc5f824b4fbb57046
Yuan Ling <yualing@amazon.com>
2023-08-23 17:57:44 -0700

    display issue

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navList = document.querySelector(".nav-list");
+
+    menuToggle.addEventListener("click", function () {
+        navList.classList.toggle("active");
+    });
+});
